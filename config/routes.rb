0f08fd8397f2ca6eaa9f2708ae6fdd34094b7b27
Ivan Lučev <lucev.ivan@gmail.com>
2016-02-09 18:06:14 +0100

    Change devise sign in path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'pirates', path_names: {sign_in: 'boarding' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
