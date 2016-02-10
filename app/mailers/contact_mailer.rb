3d9e0a261b844f26c55fb8d5181f299d4e2e6ef1
Ivan Lučev <lucev.ivan@gmail.com>
2016-02-10 15:35:59 +0100

    Fix email address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   default from: 'contact@refactorit.co'
   def contact_email(contact)
     @contact = contact
-    mail(to: "contact@refactor.co", subject: "#{@contact.name} has a question")
+    mail(to: "contact@refactorit.co", subject: "#{@contact.name} has a question")
   end
 end
