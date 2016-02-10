58b778ac318d87c01aaecb8d72309ea757acead7
NevenRakonic <neven.rakonic@gmail.com>
2016-02-10 16:00:06 +0100

    mailer spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,5 +3,11 @@ FactoryGirl.define do
     sequence(:email) { |n| "person_#{n}@example.com" }
     password 'password'
   end
+
+  factory :contact_form do
+    sequence(:email) { |n| "person_#{n}@example.com" }
+    sequence(:name)  { |n| "Person no.#{n}"}
+    message "This is a message!"
+  end
 end
 
