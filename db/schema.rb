90f64ddf58825bb446bdb283348718a45ee1ea4e
NevenRakonic <neven.rakonic@gmail.com>
2016-02-15 12:56:06 +0100

    add slug to post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211113327) do
+ActiveRecord::Schema.define(version: 20160215115453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160211113327) do
     t.integer  "author_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
     t.index ["author_id"], name: "index_posts_on_author_id", using: :btree
   end
 
