e01adafbf25e8a65868d68ed54701fb04b421b87
NevenRakonic <neven.rakonic@gmail.com>
2016-02-15 13:20:46 +0100

    add slug helper methods and validations on post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,19 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  validates :title, :body, presence: true
+
+  before_save :assign_slug
+  before_save :strip_title
+
+  def assign_slug
+    self.slug ||= title_to_slug
+  end
+
+  def strip_title
+    self.title = self.title.strip
+  end
+
+  def title_to_slug
+    title.downcase.gsub(/\s+/, "-")
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,6 +7,37 @@ RSpec.describe Post, :type => :model do
     end
 
     it { should belong_to(:author) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+  end
+
+  describe "#strip_title" do
+    let(:post) { FactoryGirl.build(:post) }
+
+    it "strips whitespace around strings" do
+      post.title = "   Title with a bunch of whitespace around    "
+      expect(post.strip_title).to eq "Title with a bunch of whitespace around"
+    end
+
+    it "title is stripped before saving" do
+      post.title = "   Title with a bunch of whitespace around    "
+      post.save
+      expect(post.title).to eq "Title with a bunch of whitespace around"
+    end
+  end
+
+  describe "#title_to_slug" do
+    let(:post) { FactoryGirl.build(:post) }
+
+    it "returns correctly slugified text" do
+      post.title = "This is a rails 5 text"
+      expect(post.title_to_slug).to eq "this-is-a-rails-5-text"
+    end
+
+    it "returns correctly slugified text with multiple whitespace" do
+      post.title = "Title WIth    whitespace"
+      expect(post.title_to_slug).to eq "title-with-whitespace"
+    end
   end
 end
 
