c262352b43812bf578a1053b7121a35ba1dae9f0
NevenRakonic <neven.rakonic@gmail.com>
2016-03-04 12:26:40 +0100

    set rbenv for capistrano and add unicorn config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,8 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'seaseuropa.eu', user: 'deploy', roles: %w{web app}
+server 'seaseuropa.eu', user: 'nrakonic', roles: %w{web app}
+# server 'seaseuropa.eu', user: 'ilucev', roles: %w{web app}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
