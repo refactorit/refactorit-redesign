8bc4f24f47ffb71c1c93d298a02c8c88b7d22bc6
Ivan Lučev <ivan.lucev@refactorit.co>
2016-03-29 15:45:04 +0200

    Rename dribbble image url column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329105131) do
+ActiveRecord::Schema.define(version: 20160329133946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20160329105131) do
     t.integer  "views_count"
     t.integer  "likes_count"
     t.string   "html_url"
-    t.string   "teaser_image_url"
+    t.string   "image_url"
     t.text     "response_body"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
