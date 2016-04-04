abdb4586188e0ab8fc84cd372af6c78710b03e56
NevenRakonic <neven.rakonic@gmail.com>
2016-04-04 13:29:15 +0200

    rebase and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160401153607) do
 
   # These are extensions that must be enabled in order to support this database
@@ -24,8 +23,8 @@ ActiveRecord::Schema.define(version: 20160401153607) do
     t.string   "html_url"
     t.string   "image_url"
     t.text     "response_body"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "posts", force: :cascade do |t|
