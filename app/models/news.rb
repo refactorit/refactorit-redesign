73bccd9bda0aac3e90a3af00b57b32f5ede7c1fd
NevenRakonic <neven.rakonic@gmail.com>
2016-04-06 17:54:36 +0200

    update publish date before saving news

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,4 +2,11 @@ class News < ApplicationRecord
   validates :title, :description, presence: true
 
   scope :published, -> { where(published: true) }
+  before_save :update_published_date
+
+  def update_published_date
+    if published? && published_at.nil?
+      self.published_at = Time.now
+    end
+  end
 end
