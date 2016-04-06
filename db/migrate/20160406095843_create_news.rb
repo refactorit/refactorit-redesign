3e5ba6c4d46a29c9f08a415b557416c3b49ee378
NevenRakonic <neven.rakonic@gmail.com>
2016-04-06 12:10:05 +0200

    modify new migration

--- db/migrate/20160406095843_create_news.rb
+++ db/migrate/20160406095843_create_news.rb
@@ -1,10 +1,11 @@
 class CreateNews < ActiveRecord::Migration[5.0]
   def change
     create_table :news do |t|
-      t.string     :title, null: false
-      t.string     :description, null: false
-      t.string     :published_at
-      t.string     :url
+      t.string       :title, null: false
+      t.string       :description, null: false
+      t.datetime     :published_at
+      t.boolean      :published
+      t.string       :url
 
       t.timestamps
     end
