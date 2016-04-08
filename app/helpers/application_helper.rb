8760f8cbbdbc093df0cf84a7537c253c38cfc188
NevenRakonic <neven.rakonic@gmail.com>
2016-04-08 14:02:32 +0200

    add social and author links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,24 @@ module ApplicationHelper
       posts_path
     end
   end
+
+  def refactorit_twitter_url
+    "https://twitter.com/refactorit/"
+  end
+
+  def refactorit_facebook_url
+    "https://www.facebook.com/refactorit.co"
+  end
+
+  def refactorit_github_url
+    "https://github.com/refactorit"
+  end
+
+  def refactorit_linkedin_url
+    "https://www.linkedin.com/company/3495144"
+  end
+
+  def refactorit_dribbble_url
+    "https://dribbble.com/refactorit"
+  end
 end
