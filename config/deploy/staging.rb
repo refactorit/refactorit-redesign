5b4af915b0b610fd3d566277242306a84dacff1c
Ivan Lučev <ivan.lucev@refactorit.co>
2016-04-11 12:57:27 +0200

    Update capistrano configuration

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -15,10 +15,11 @@
 # something that quacks like a hash can be used to set
 # extended properties on the server.
 server '146.185.128.78', user: 'nrakonic', roles: %w{web db}
-# server 'seaseuropa.eu', user: 'ilucev', roles: %w{web app}
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/srv/www/refactorit-production'
+set :deploy_to, '/srv/www/refactorit'
+
+# server 'seaseuropa.eu', user: 'ilucev', roles: %w{web app}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
