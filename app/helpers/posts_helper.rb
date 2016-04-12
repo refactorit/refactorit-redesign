d0c7baccb44f5ff9262ce6f44b946b2b2c927143
NevenRakonic <neven.rakonic@gmail.com>
2016-04-12 19:29:40 +0200

    fix blog order, add previews on blog index

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,4 +3,9 @@ module PostsHelper
   def markdown_to_html(text)
     MarkdownRenderer.new.render_text(text)
   end
+
+  # takes in ActiveSupport::TimeWithZone
+  def format_date(date)
+    date.strftime(" %d / %m / %Y")
+  end
 end
