56271b676d30ecbc4b1d829155f4b361353d591c
NevenRakonic <neven.rakonic@gmail.com>
2016-04-13 14:30:09 +0200

    create image model and add paperclip to it

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406095843) do
+ActiveRecord::Schema.define(version: 20160413122907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attachments", force: :cascade do |t|
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "dribbble_shots", force: :cascade do |t|
     t.string   "title"
