35656bd82adcc187be056266fd7a1622b1ae8448
NevenRakonic <neven.rakonic@gmail.com>
2016-04-14 11:19:43 +0200

    test attachments

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Attachment, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'should have a valid factory for image attachments' do
+      FactoryGirl.build(:attachment)
+    end
+
+    it { should have_attached_file(:image) }
+    it { should validate_attachment_presence(:image) }
+    it { should validate_attachment_content_type(:image).
+                  allowing('image/png', 'image/gif').
+                  rejecting('text/plain', 'text/xml') }
+  end
+
 end
