8f4dfb4ba45cf6e4a6a346a5fcaf106b42d3850f
NevenRakonic <neven.rakonic@gmail.com>
2016-04-28 11:26:51 +0200

    add titles to pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -6,11 +6,6 @@ describe PagesController do
     expect(response).to have_http_status 200
   end
 
-  it "#about responds with 200" do
-    get :about
-    expect(response).to have_http_status 200
-  end
-
   it "#team responds with 200" do
     get :team
     expect(response).to have_http_status 200
